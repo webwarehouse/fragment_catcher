c22ac1b582c8072b1f3506ed14f36380b27a878f
Roman <gambro87@gmail.com>
2017-07-10 10:29:19 +0300

    убрал излишний модуль

--- lib/fragment_catcher/client.rb
+++ lib/fragment_catcher/client.rb
@@ -1,55 +1,53 @@
 require 'nokogiri'
 require 'open-uri'
 
-module FragmentCatcher
-  class Client
-    attr_accessor :url, :css
+class Client
+  attr_accessor :url, :css
 
-    def initialize(url: nil, css: nil)
-      @url = url
-      @css = css
-    end
-
-    def get
-      if success?
-        true
-      else
-        false
-      end
-    end
-
-    def fragments
-      get_fragments = Array.new
-      page.css(css).each do |link|
-        get_fragments << link.content
-      end
-      get_fragments
-    end
-
-    def success?
-      unless fragments.empty?
-        true
-      else
-        false
-      end
-    end
-
-    def body
-      page.at('body').inner_html
-    end
-
-    def code
-      file.status.first
-    end
-
-    private
-
-      def page
-        @page ||= Nokogiri::HTML(file)
-      end
-
-      def file
-        @file ||= open(url)
-      end
+  def initialize(url: nil, css: nil)
+    @url = url
+    @css = css
   end
+
+  def get
+    if success?
+      true
+    else
+      false
+    end
+  end
+
+  def fragments
+    get_fragments = Array.new
+    page.css(css).each do |link|
+      get_fragments << link.content
+    end
+    get_fragments
+  end
+
+  def success?
+    unless fragments.empty?
+      true
+    else
+      false
+    end
+  end
+
+  def body
+    page.at('body').inner_html
+  end
+
+  def code
+    file.status.first
+  end
+
+  private
+
+    def page
+      @page ||= Nokogiri::HTML(file)
+    end
+
+    def file
+      @file ||= open(url)
+    end
 end
